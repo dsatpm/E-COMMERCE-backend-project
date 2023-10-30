aa0f1de99fc8afba6015a86872ef380468d60d71
dsatpm <jeremiahjh@gmail.com>
2023-10-29 22:16:04 -0700

    lint/add comments to server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// Main entry point for the server
 const express = require('express');
 const routes = require('./routes');
 const Sequelize = require('./config/connection');
 
+// Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync Sequelize models to the database, then turn on the server
 Sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
