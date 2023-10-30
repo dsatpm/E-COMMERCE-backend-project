1e8c4ca5d853c519ee912a48022c04dd3559f5df
dsatpm <jeremiahjh@gmail.com>
2023-10-29 22:12:09 -0700

    add comments to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
+// Establishes connection to database
 require('dotenv').config();
-
+// Import Sequelize constructor
 const Sequelize = require('sequelize');
 
+// Create connection to our database, pass in MySQL info for username and password
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
